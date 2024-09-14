8af4329a26e9e84d6be3c05121a39a0b98d721f0
Ellie <ecsymonds@gmail.com>
2024-09-14 14:12:40 +0100

    adds completed caesar cipher task

--- tasks/caesar_cipher.py
+++ tasks/caesar_cipher.py
@@ -1,20 +1,22 @@
 
+"""Create a function which takes the user's message and encodes or decodes it by shifting
+the letters by the amount determined by the user"""
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-# text = input("Type your message:\n")
-# shift = input("Type the shift number:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
-    original_text = original_text.lower()
-    shift_amount = int(shift_amount)
-
-    encrypted_string = ""
+def caesar_cipher(original_text, shift_amount, encode_or_decode):
+    output_text = ""
     for letter in original_text:
-        new_position = alphabet.index(letter) + shift_amount
-        new_position %= len(alphabet)
-        encrypted_string += alphabet[new_position]
-    return f"Here is the encoded result: {encrypted_string}"
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+        shift_amount %= len(alphabet)
+        output_text += alphabet[shift_amount]
+    print(f"Here is the {direction}ed result: {output_text}")
 
 
+caesar_cipher(text, shift, direction)
